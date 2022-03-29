6eebadd1ab585e2bd4d1b7f50b525d68397d4faa
Daniel Stephen <daniel.vergil2711@gmail.com>
2022-03-28 18:53:08 -0700

    GraphiQl Client

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -69,3 +69,161 @@ export const ADD_REACTION = gql`
     }
   }
 `;
+
+export const ADD_NBA_THOUGHT = gql`
+  mutation addNbaThought($thoughtText: String!) {
+    addNbaThought(thoughtText: $thoughtText) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+      }
+    }
+  }
+`;
+
+
+export const ADD_NBA_REACTION = gql`
+  mutation addNbaReaction($thoughtId: ID!, $reactionBody: String!) {
+    addNbaReaction(thoughtId: $thoughtId, reactionBody: $reactionBody) {
+      _id
+      reactionCount
+      reactions {
+        _id
+        reactionBody
+        createdAt
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_NFL_THOUGHT = gql`
+  mutation addNflThought($thoughtText: String!) {
+    addNflThought(thoughtText: $thoughtText) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+      }
+    }
+  }
+`;
+
+
+export const ADD_NFL_REACTION = gql`
+  mutation addNflReaction($thoughtId: ID!, $reactionBody: String!) {
+    addNflReaction(thoughtId: $thoughtId, reactionBody: $reactionBody) {
+      _id
+      reactionCount
+      reactions {
+        _id
+        reactionBody
+        createdAt
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_MLB_THOUGHT = gql`
+  mutation addMlbThought($thoughtText: String!) {
+    addMlbThought(thoughtText: $thoughtText) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+      }
+    }
+  }
+`;
+
+
+export const ADD_MLB_REACTION = gql`
+  mutation addMlbReaction($thoughtId: ID!, $reactionBody: String!) {
+    addMlbReaction(thoughtId: $thoughtId, reactionBody: $reactionBody) {
+      _id
+      reactionCount
+      reactions {
+        _id
+        reactionBody
+        createdAt
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_MLS_THOUGHT = gql`
+  mutation addMlsThought($thoughtText: String!) {
+    addMlsThought(thoughtText: $thoughtText) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+      }
+    }
+  }
+`;
+
+
+export const ADD_MLS_REACTION = gql`
+  mutation addMlsReaction($thoughtId: ID!, $reactionBody: String!) {
+    addMlsReaction(thoughtId: $thoughtId, reactionBody: $reactionBody) {
+      _id
+      reactionCount
+      reactions {
+        _id
+        reactionBody
+        createdAt
+        username
+      }
+    }
+  }
+`;
+
+
+export const ADD_NHL_THOUGHT = gql`
+  mutation addNhlThought($thoughtText: String!) {
+    addNhlThought(thoughtText: $thoughtText) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+      }
+    }
+  }
+`;
+
+
+export const ADD_NHL_REACTION = gql`
+  mutation addNhlReaction($thoughtId: ID!, $reactionBody: String!) {
+    addNhlReaction(thoughtId: $thoughtId, reactionBody: $reactionBody) {
+      _id
+      reactionCount
+      reactions {
+        _id
+        reactionBody
+        createdAt
+        username
+      }
+    }
+  }
+`;
+
+
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -36,6 +36,186 @@ export const QUERY_THOUGHT = gql`
   }
 `;
 
+export const QUERY_NBA_THOUGHTS = gql`
+  query nbaThoughts($username: String) {
+    nbaThoughts(username: $username) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
+
+export const QUERY_NBA_THOUGHT = gql`
+  query nbaThought($id: ID!) {
+    nbaThought(_id: $id) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
+
+export const QUERY_NFL_THOUGHTS = gql`
+  query nflThoughts($username: String) {
+    nflThoughts(username: $username) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
+
+export const QUERY_NFL_THOUGHT = gql`
+  query nflThought($id: ID!) {
+    nflThought(_id: $id) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
+
+export const QUERY_MLB_THOUGHTS = gql`
+  query mlbThoughts($username: String) {
+    mlbThoughts(username: $username) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
+
+export const QUERY_MLB_THOUGHT = gql`
+  query mlbThought($id: ID!) {
+    mlbThought(_id: $id) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
+
+export const QUERY_MLS_THOUGHTS = gql`
+  query mlsThoughts($username: String) {
+    mlsThoughts(username: $username) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
+
+export const QUERY_MLS_THOUGHT = gql`
+  query mlsThought($id: ID!) {
+    mlsThought(_id: $id) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
+
+export const QUERY_MLS_THOUGHTS = gql`
+  query nhlThoughts($username: String) {
+    nhlThoughts(username: $username) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
+
+export const QUERY_NHL_THOUGHT = gql`
+  query nhlThought($id: ID!) {
+    nhlThought(_id: $id) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
+
 export const QUERY_USER = gql`
   query user($username: String!) {
     user(username: $username) {
